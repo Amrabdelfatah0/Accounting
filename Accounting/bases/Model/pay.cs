f91d54f803d97c363fc91913d02c5328083061f4
Amr Mohamed <88450778+Amrabdelfatah0@users.noreply.github.com>
2022-01-10 23:06:53 +0200

    new updite

---
+++ Accounting/bases/Model/pay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.bases.Model
+{
+    public class pay:BaseWithDate
+    {
+        public Clientscs clientscs { get; set; }
+        public bool PaymentType { get; set; }
+        public double Amount { get; set; }
+        public bool Journal { get; set; }
+
+
+    }
+}
